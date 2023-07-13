ad1265c8263a42948fde615a4fc8ff0663d976ae
iterepa <wizmond705@gmail.com>
2023-07-13 15:33:13 +0300

    added exception to getAllQuestions method

--- src/main/java/com/frogiri/demo/controller/QuestionController.java
+++ src/main/java/com/frogiri/demo/controller/QuestionController.java
@@ -3,6 +3,8 @@ package com.frogiri.demo.controller;
 import com.frogiri.demo.Model.Question;
 import com.frogiri.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +16,7 @@ public class QuestionController {
     @Autowired
     QuestionService questionService;
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
 
--- src/main/java/com/frogiri/demo/service/QuestionService.java
+++ src/main/java/com/frogiri/demo/service/QuestionService.java
@@ -3,8 +3,12 @@ package com.frogiri.demo.service;
 import com.frogiri.demo.Model.Question;
 import com.frogiri.demo.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,8 +16,13 @@ public class QuestionService {
 
     @Autowired
     QuestionDao questionDao;
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
     public List<Question> getQuestionsByCategory(String category) {
